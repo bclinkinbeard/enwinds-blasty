2995f60e2eca808a286ea6bb376922a054730da9
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-07-05 15:38:59 -0400

    Update literasee-logo.js

--- components/literasee-logo.js
+++ components/literasee-logo.js
@@ -4,9 +4,9 @@ const IdyllComponent = require('idyll-component');
 class Logo extends IdyllComponent {
   render() {
     return (
-      <div className={'logo-lockup'}>
+      <div className="logo-lockup">
         <a href="https://literasee.io/">
-          <img className={'logo'} src={'leaf.svg'} />
+          <img className="logo" src="leaf.svg" />
           <small>Made with Literasee</small>
         </a>
       </div>
